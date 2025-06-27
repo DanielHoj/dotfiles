28ebd8906c3822d1ac779f8322045e9e4fb63b1c
DanielH <danielhoj1990@gmail.com>
2025-06-27 07:53:28 +0200

    enable lsp for snacks

--- nvim/.config/nvim/lua/plugins/snacks.lua
+++ nvim/.config/nvim/lua/plugins/snacks.lua
@@ -2,7 +2,7 @@ return {
   "folke/snacks.nvim",
   priority = 1000,
   lazy = false,
-  ---@type snacks.Config
+  ---@module "snacks"
   opts = {
     bigfile = { enabled = true },
     dashboard = { enabled = true },
